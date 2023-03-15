4b4f24c9333dcff24948d35c2a57b27d50f45b95
derango25 <zyko25@gmail.com>
2023-03-16 01:43:27 +0300

    сделан блог, ссылки на новости блога

--- personal_portfolio/blog/urls.py
+++ personal_portfolio/blog/urls.py
@@ -4,8 +4,11 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.all_blogs, name = 'all_blogs' ),
+    path('<int:blog_id>/', views.detail, name = 'detail' ),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
